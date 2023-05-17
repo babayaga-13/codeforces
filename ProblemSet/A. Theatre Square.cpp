2d7be3b32c49078b7ada374f20872b8add76565f
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2023-05-17 20:39:34 +0600

    Create A. Theatre Square.cpp

---
+++ ProblemSet/A. Theatre Square.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long long s;
+    double n,m,a;
+    cin>>n>>m>>a;
+    n=ceil(n/a);
+    m=ceil(m/a);
+    s= n*m ;
+    cout<<s;
+    return 0;
+}
