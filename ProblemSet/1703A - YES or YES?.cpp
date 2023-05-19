3c3679c18cb4b14cea2f4522856d9d0c7530a4f3
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2023-05-20 02:11:58 +0600

    Create 1703A - YES or YES?.cpp

---
+++ ProblemSet/1703A - YES or YES?.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+{
+string s;
+string s1="yes";
+
+for(int i=0;i<3;i++)
+    {cin>>s[i];
+    s[i]=tolower(s[i]);
+    }
+    if(s1[0]==s[0]&&s1[1]==s[1]&&s1[2]==s[2])
+        cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+}
+}
+
+
