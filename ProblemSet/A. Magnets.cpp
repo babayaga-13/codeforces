9675053bdbef139eabe6f3e0ab951573d4836483
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2023-05-20 20:35:24 +0600

    Create A. Magnets.cpp

---
+++ ProblemSet/A. Magnets.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int n,c=1;
+ cin>>n;
+    int a[n];
+    cin>>a[0];
+    for(int i=1;i<n;i++)
+    {
+        cin>>a[i];
+        if(a[i]!=a[i-1])
+        {
+            c++;
+    }
+    }
+cout<<c;
+}
+
+
