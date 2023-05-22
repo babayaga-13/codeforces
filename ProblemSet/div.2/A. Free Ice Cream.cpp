2e31e593fa32c1ab9bddcd12ff32039fec1b8f05
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2023-05-22 21:55:54 +0600

    Create A. Free Ice Cream.cpp

---
+++ ProblemSet/div.2/A. Free Ice Cream.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   long long t,s,x=0;
+   cin>>t>>s;
+   while(t--)
+   {
+     char c;
+     long long n;
+     cin>>c>>n;
+     if(c=='-'&&s<n)
+       x++;
+     else if(c=='-'&&s>=n)
+        s-=n;
+     else
+        s+=n;
+
+   }
+   cout<<s<<" "<<x;
+}
+
+div
