ace4b51f0872eba86ae418d732998ef8ecf526e0
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2023-06-03 02:02:34 +0600

    Create 	630C - Lucky Numbers.cpp

---
+++ ProblemSet/630C - Lucky Numbers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n; long long s=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+       s+=pow(2,i);
+
+    }
+    cout<<s;
+    }
