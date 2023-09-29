53ee3e9c391dd4ee6c9c834bd2aa415103d3134d
babayaga-13 <133708548+babayaga-13@users.noreply.github.com>
2023-09-30 00:59:34 +0600

    Create 	1873D - 1D Eraser.cpp

---
+++ ProblemSet/div.4/1873D - 1D Eraser.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k,c=0;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='B')
+            {
+                c++;
+                i+=(k-1);
+            }
+        }
+    cout<<c<<endl;
+    }
+}
